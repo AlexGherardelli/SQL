f0c486c86cb07aaf7ac80ecdaa7db1dc45c90cd0
Alessandra Gherardelli <alessandra.gherardelli@gmail.com>
2021-03-31 21:15:02 +0200

    Q7/9 Stanford SQL social media exercise, updated README

--- social_query.sql
+++ social_query.sql
@@ -22,3 +22,23 @@ ORDER by h1.name
 SELECT DISTINCT h.name, h.grade FROM Highschooler h
 WHERE NOT EXISTS (SELECT * FROM Likes WHERE h.ID = Likes.ID1 OR h.ID = Likes.ID2)
 ORDER BY h.grade, h.name
+
+--  Q5: For every situation where student A likes student B, but we have no information about whom B likes (that is, B does not appear as an ID1 in the Likes table), return A and B's names and grades. 
+SELECT h1.name, h1.grade, h2.name, h2.grade
+FROM Highschooler as h1, Highschooler as h2, Likes
+WHERE (h1.ID = Likes.ID1 AND h2.ID = Likes.ID2) AND h2.ID NOT IN (SELECT ID1 FROM Likes)
+
+-- Q6:  Find names and grades of students who only have friends in the same grade. Return the result sorted by grade, then by name within each grade. 
+
+SELECT name, grade
+FROM Highschooler
+WHERE ID NOT IN (SELECT ID1 FROM Friend f, Highschooler h1, Highschooler h2
+WHERE h1.ID = f.ID1 AND h2.ID = f.ID2 AND h1.grade <> h2.grade)
+ORDER BY grade, name
+
+-- Q7:  For each student A who likes a student B where the two are not friends, find if they have a friend C in common (who can introduce them!). For all such trios, return the name and grade of A, B, and C. 
+
+SELECT  A.name, A.grade, B.name, B.grade, C.name, C.grade
+FROM Highschooler A, Highschooler B, Highschooler C, Likes l, Friend f1, Friend f2
+WHERE (A.ID = l.ID1 AND B.ID = l.ID2) and B.ID NOT IN (SELECT ID2 FROM Friend WHERE ID1 = A.ID)
+AND (A.ID = f1.ID1 AND C.ID = f1.ID2) AND (B.ID = f2.ID1 AND C.ID = f2.ID2)
